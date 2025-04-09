e25740684647a6899c0684d3756d558b70d32f75
Jean Michell Ritter <michelld777@gmail.com>
2025-04-09 09:22:35 -0300

    Colocando todos os dados no GitHub

---
+++ app/routes/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
+      <h1>Olá, mundo!</h1>
+      <p>Esta é minha primeira aplicação React 🎉</p>
+    </div>
+  );
+}
+
+export default App;
